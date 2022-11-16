e85cc17a74c637f73d1cd05a97f534fd8be78a88
SAJAL BRAHMA <100282172+SajalBrahma@users.noreply.github.com>
2022-11-16 11:43:50 +0530

    Update Binary_Search_Tree_Operation.cpp

--- Binary_Search_Tree_Operation.cpp
+++ Binary_Search_Tree_Operation.cpp
@@ -79,12 +79,12 @@ int main()
 		root=insert(root,element);
 	}
 	printf("Inorder Traversal :- ");
-    inorder(root);
-    printf("\nPreorder Traversal :- ");
-    preorder(root);
-    printf("\nPostorder Traversal :- ");
-    postorder(root);
-    return 0;
+        inorder(root);
+        printf("\nPreorder Traversal :- ");
+        preorder(root);
+        printf("\nPostorder Traversal :- ");
+        postorder(root);
+        return 0;
 }
 
 // Input -> 8 3 1 6 7 10 14 4
